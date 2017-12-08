06ad530d1a1f7f5d816e00405ac3875bc806b28c
ledalee <david_s_lee@manulife.com>
2017-12-08 10:26:16 -0500

    add keycloak init method to authenticate

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { keycloak } from '../core/mva-keycloak';
 import * as _ from 'lodash';
 
 @Component({
-  selector: 'mva-home',
+  selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
@@ -18,10 +18,18 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     let _keycloak = keycloak(environment.ssoConfig);
     
-    (<any>_keycloak).init({onLoad: 'login-required', flow: 'standard', responseMode: 'query'})
+    (<any>_keycloak)
+      .init({onLoad: 'login-required', flow: 'standard', responseMode: 'query'})
       .success(authed => {
         if (authed) {
-          this.localStorage.set('auth', _.pick((<any>_keycloak).tokenParsed, ['user_id','user_name','email','auth_time','exp']));
+          let auth = _.pick((<any>_keycloak).tokenParsed, ['user_id','user_name','email','auth_time','exp']);
+          this.localStorage.set('auth', auth);
+          console.log('User ID: ', auth.user_id);
+          console.log('User Name: ', auth.user_name);
+
+          // TODO save into state, 
+          // TODO if need, save request url and redirect after login
+
           this.router.navigate(['/members']);
         } else {
           console.log('401 error');
