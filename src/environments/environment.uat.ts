8ad54b80d2d1d3dcf4fa188075d2914d24661636
David Lee <david_s_lee@manulife.com>
2018-04-25 18:43:17 -0400

    added more environment files for UAT and QA

---
+++ src/environments/environment.uat.ts
@@ -0,0 +1,38 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+const apiBase = 'https://vitalitymemberservice-dev.apps.cac.preview.pcf.manulife.com/v1';
+
+export const environment = {
+  production: false,
+  emptyEmailString: '** required **',
+  dateFormat: 'DD/MMM/YYYY',
+  serverDateFormat: 'YYYY-MM-DD',
+  endPoints: {
+    memberList: `${apiBase}/members`,
+    member: `${apiBase}/member`,
+    auditLog: `${apiBase}/audit`,
+    lookups: `${apiBase}/lookups`,
+    roles: `${apiBase}/roles`,
+    newApi: `daafdfafadfs`
+  },
+  ssoConfig: {
+    url: 'https://manulife-dev.login.sys.cac.preview.pcf.manulife.com/oauth/authorize',
+    urlToken: 'https://manulife-dev.login.sys.cac.preview.pcf.manulife.com/oauth/token',
+    clientId: '4a048376-c719-4f89-99e8-93dbdaa63528',
+    credentials: {
+      secret: '287aba65-65a9-4284-b57b-421041d7d6f1'
+    }
+  },
+  lookups: {
+    genders: [],
+    customerRoles: [],
+    language: [],
+    memberStatus: [],
+    provinces: [],
+    cotinines: [],
+    branches: []
+  }
+};
