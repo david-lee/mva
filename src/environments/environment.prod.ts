59af39b583c755b939ed9654f6cc77026c5a6f3d
Sandi Ng <MAT1603@americas.manulife.net>
2018-07-05 13:54:33 -0400

    env update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-const apiBase = 'https://vitalitymemberservice.apps.cac.pcf.manulife.com/v1';
+const apiBase = 'https://vitalitymemberservice-uat.apps.cac.pcf.manulife.com/v1';
 
 export const environment = {
   production: false,
